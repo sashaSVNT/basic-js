2a900eb89498c6dbb1689cb85d88f518a2bee213
Aliaksandr Iluishenka <sasha29292931@gmail.com>
2023-04-18 02:00:44 +0300

    feat: solve some questions

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,19 +1,18 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Given two strings, find the number of common characters between them.
- *
- * @param {String} s1
- * @param {String} s2
- * @return {Number}
- *
- * @example
- * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
- * Strings have 3 common characters - 2 "a"s and 1 "c".
- */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  const firstChars = s1.split('');
+  for (let i = 0; i < s2.length; i++) {
+    let charOfSecondString = s2[i];
+    let charIndex = firstChars.indexOf(charOfSecondString);
+    if (charIndex !== -1) {
+      count++;
+      firstChars.splice(charIndex, 1)
+    }
+  }
+  return count;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -2,40 +2,27 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 
 function getSeason(date) {
-  if (!date || isNaN(date.getTime())) {
-    throw new Error();
-  }
   if (!date) {
     return 'Unable to determine the time of year!';
   }
-  const month = date.getMonth();
-  let season;
-  switch (month) {
-    case 11:
-    case 0:
-    case 1:
-      season = `winter`;
-      break;
-    case 2:
-    case 3:
-    case 4:
-      season = `spring`;
-      break;
-    case 5:
-    case 6:
-    case 7:
-      season = `summer`;
-      break;
-    case 8:
-    case 9:
-    case 10:
-      season = `autumn`;
-      break;
-    default:
-      return 'Unable to determine the time of year!';
+  if (!(date instanceof Date && !isNaN(date))) {
+    throw new Error('Invalid date!');
   }
-  return season;
+
+  const currentMonth = date.getMonth();
+  if (currentMonth >= 2 && currentMonth <= 4) {
+    return `spring`;
+  }
+  else if (currentMonth >= 5 && currentMonth <= 7) {
+    return `summer`;
+  }
+  else if (currentMonth >= 8 && currentMonth <= 10) {
+    return `autumn`;
+  }
+  else
+    return `winter`;
 }
+
 
 module.exports = {
   getSeason
